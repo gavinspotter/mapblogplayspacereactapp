a3d138a2cc436c7a0048c57b7b97ff4627a829db
Osiris <potterosiris@gmail.com>
2020-10-08 18:29:54 -0400

    input.js component & css

--- src/shared/FormComponents/Input.js
+++ src/shared/FormComponents/Input.js
@@ -1,5 +1,19 @@
 import React from "react";
 
-const Input = () => {};
+const Input = (props) => {
+  const element =
+    props.element === "input" ? (
+      <input id={props.id} type={props.type} placeholder={props.placeholder} />
+    ) : (
+      <textarea id={props.id} rows={props.rows || 3} />
+    );
+
+  return (
+    <div>
+      <label htmlFor={props.id}>{props.label}</label>
+      {element}
+    </div>
+  );
+};
 
 export default Input;
