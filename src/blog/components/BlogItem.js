a513024ff09d14cdaf48497f67b943fe5c770779
Osiris <potterosiris@gmail.com>
2020-10-08 18:33:22 -0400

    blog item .js

--- src/blog/components/BlogItem.js
+++ src/blog/components/BlogItem.js
@@ -1,5 +1,22 @@
 import React from "react";
+import Button from "../../shared/FormComponents/Button";
+import Card from "../../shared/UIElements/Card";
 
-const BlogItem = () => {};
+const BlogItem = (props) => {
+  return (
+    <li className="place-item">
+      <Card className="place-item__content">
+        <div>
+          <div className="place-item__image">
+            <img src={props.imge} alt={props.blog} />
+          </div>
+          <div>{props.blog}</div>
+        </div>
+        <Button to={`/blog/${props.id}`}>edit</Button>
+        <Button>delete</Button>
+      </Card>
+    </li>
+  );
+};
 
 export default BlogItem;
