4bb69087a6321be831af6ad8e1a2ef01529c7dee
Osiris <potterosiris@gmail.com>
2020-10-08 18:20:37 -0400

    router app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import "./App.css";
+import MainNavigation from "./shared/navigation/MainNavigation";
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact></Route>
+          <Route path="/:userId/blog" exact></Route>
+          <Route path="/blog/new" exact></Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
--- src/shared/navigation/MainNavigation.js
+++ src/shared/navigation/MainNavigation.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import NavBar from "./Navbar";
 import MainHeader from "./MainHeader";
+import "./MainNavigation.css";
 
 const MainNavigation = () => {
   return (
