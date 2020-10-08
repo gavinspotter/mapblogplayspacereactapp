a509228a8006a3a71d6077cfe395ba0db0dd45f6
Osiris <potterosiris@gmail.com>
2020-10-08 18:02:07 -0400

    main navigation js component

--- src/shared/navigation/MainNavigation.js
+++ src/shared/navigation/MainNavigation.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import NavBar from "./Navbar";
+import MainHeader from "./MainHeader";
 
-const MainNavigation = () => {};
+const MainNavigation = () => {
+  return (
+    <MainHeader>
+      <button className="main-navigation__menu-btn">
+        <span />
+        <span />
+        <span />
+      </button>
+      <h1 className="main-navigation__title">
+        <Link to="/">Your Places</Link>
+      </h1>
+      <nav className="main-navigation__header-nav">
+        <NavBar />
+      </nav>
+    </MainHeader>
+  );
+};
 
 export default MainNavigation;
